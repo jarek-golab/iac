dd3d6b029faaa1b8681e97fa45b3844fd03fcac6
Jarek Golab <jarekgolab@MacBook-Pro-Jarek.local>
2022-09-23 16:05:25 +0200

    cloudformation - jenkins

--- edmc-neptun/ad-hoc/ec2/commands/jenkins-master.sh
+++ edmc-neptun/ad-hoc/ec2/commands/jenkins-master.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-PROJECT="environments"
+PROJECT="ad-hoc"
 STAGE="devops"
 
 
 #########  Params ###########
 
-COMPONENT="EC2"
+COMPONENT="ec2"
 STACK="jenkins-master"
-TEMPLATE="jenkins-master"
+TEMPLATE="jenkins"
 PARAMETERS="jenkins-master"
 ENVIRONMENT="LearnEnv"
 
@@ -16,16 +16,14 @@ ENVIRONMENT="LearnEnv"
 ######### common part #########
 
 TEMPLATE_FILE="$PROJECT/$COMPONENT/templates/$TEMPLATE.yaml"
-PARAM_FILE="$PROJECT/parameters/parameters.json"
-PARAMS=$(cat $PARAM_FILE | jq -jr 'map("\(.ParameterKey)=\(.ParameterValue)") | join (" ")')
+#PARAM_FILE="$PROJECT/parameters/parameters.json"
+#PARAMS=$(cat $PARAM_FILE | jq -jr 'map("\(.ParameterKey)=\(.ParameterValue)") | join (" ")')
 
 deploy="aws cloudformation deploy \
     --template-file $TEMPLATE_FILE \
     --stack-name $PROJECT-$COMPONENT-$STACK \
     --no-fail-on-empty-changeset \
-    --parameter-overrides $PARAMS \
     --capabilities CAPABILITY_NAMED_IAM \
-    --region $REGION \
     --tags Project=$PROJECT Stage=$STAGE Component=$COMPONENT"
 
 echo $deploy
